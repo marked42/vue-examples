1c1d75f42ca31613db31eef0ea9ce4807f8de3af
penghui <pzhang2@xiaohongshu.com>
2018-05-27 23:21:55 +0800

    finish css nested counters example

--- src/components/NestedCounters.vue
+++ src/components/NestedCounters.vue
@@ -1,16 +1,19 @@
 <template>
   <div :style="counterCssRule">
-    {{ countersText }}
+    <span>
+      {{ countersText }}
+      <ol class='tooltip'>
+        <h3>counter process steps</h3>
+        <li class="counter-step-inherit">{{inheritsText}}</li>
+        <li class="counter-step-reset">{{resetsText}}</li>
+        <li class="counter-step-set">{{setsText}}</li>
+        <li class="counter-step-increment">{{incrementsText}}</li>
+        <li class="counter-step-usage">{{usageText}}</li>
+      </ol>
+    </span>
     <nested-counters v-if="children" v-for="(child, index) in children" :key="index" v-bind="child">
     </nested-counters>
 
-    <ol class='tooltip'>
-      <li class="counter-step-inherit">{{inheritsText}}</li>
-      <li class="counter-step-reset">{{resetsText}}</li>
-      <li class="counter-step-set">{{setsText}}</li>
-      <li class="counter-step-increment">{{incrementsText}}</li>
-      <li class="counter-step-use">{{countersText}}</li>
-    </ol>
   </div>
 </template>
 
@@ -28,19 +31,59 @@ export default {
     resets: Array,
     sets: Array,
     increments: Array,
+    usage: Object,
   },
   computed: {
     inheritsText() {
-      return `inherit: `
+      const { inherits } = this
+
+      const result = Object.keys(inherits).map(key => {
+        return `${key} = ${inherits[key].join('.')}`
+      })
+      return `inherits: ${result.length > 0 ? result.join(',') : 'nothing'}`
     },
     resetsText() {
-      return ','
+      const { resets } = this
+
+      const result = resets.map(({name, value}) => {
+        return `${name} ${value}`
+      })
+      return `resets: 'counter-reset: ${result.length > 0 ? result.join(' ') : 'none'};'`
     },
     setsText() {
-      return ''
+      const { sets } = this
+
+      const result = sets.map(({name, value}) => {
+        return `${name} ${value}`
+      })
+      return `sets: 'counter-set: ${result.length > 0 ? result.join(' ') : 'none'};'`
     },
     incrementsText() {
-      return ''
+      const { increments } = this
+
+      const result = increments.map(({name, value}) => {
+        return `${name} ${value}`
+      })
+      return `increments: 'counter-increment: ${result.length > 0 ? result.join(' ') : 'none'};'`
+    },
+    usageText() {
+      const { usage } = this
+
+      if (!usage) {
+        return 'usage: '
+      }
+
+      const counterUsage = !usage.counter ? ''
+        : usage.counter.map(({ name, style }) => {
+          return `counter(${name}, ${style || 'none'})`
+        }).join(' ')
+
+      const countersUsage = !usage.counters ? ''
+        : usage.counters.map(({ name, delimieter, style }) => {
+          return `counters(${name}, ${delimieter},${style || 'none'})`
+        }).join(' ')
+
+      return `usage:\n\t${counterUsage}\n\t${countersUsage}`
     },
     counterCssRule() {
       const { resets, sets, increments } = this
@@ -68,13 +111,19 @@ div
     color blue
   .tooltip
     display none
-  &:hover
-    &>.tooltip
-      display initial
-      position absolute
-      left 10%
-      padding 10px
-      background gray
-      // border 1px solid lightblue
-      list-style inside
+    width fit-content
+    z-index 1
+    li
+      white-space nowrap
+  &>span
+    position relative
+    &:hover
+      &>.tooltip
+        display initial
+        position absolute
+        left 100%
+        padding 10px
+        background gray
+        color white
+        list-style-position inside
 </style>
